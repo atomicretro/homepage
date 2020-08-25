017e7256f2b74e2ab18e79e2eabd578ff6086e61
alec <alec@joinclyde.com>
2020-08-25 18:28:15 -0400

    Use grid on home

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -5,40 +5,16 @@ import allPalettes from '../../utils/palettes';
 
 import '../../css/home.css';
 
-const firstIdx = Math.ceil(allPalettes.length / 3);
-const secondIdx = firstIdx * 2;
-const col1 = allPalettes.slice(0, firstIdx);
-const col2 = allPalettes.slice(firstIdx, secondIdx);
-const col3 = allPalettes.slice(secondIdx, allPalettes.length);
-
 const Home = ({ palette: currentPalette, _setPalette }) => {
   return (
     <div className='home'>
-      <div className='home__col home__col--one'>
+      <h2 className='home__title'>Paint the sky</h2>
+      <div className='home__grid'>
         {
-          col1.map((palette) => (
+          allPalettes.map((palette) => (
             <PaletteCard
               isSelected={ palette === currentPalette }
-              palette={ palette }
-              _setPalette={ _setPalette } />
-          ))
-        }
-      </div>
-      <div className='home__col home__col--two'>
-        {
-          col2.map((palette) => (
-            <PaletteCard
-              isSelected={ palette === currentPalette }
-              palette={ palette }
-              _setPalette={ _setPalette } />
-          ))
-        }
-      </div>
-      <div className='home__col home__col--three'>
-        {
-          col3.map((palette) => (
-            <PaletteCard
-              isSelected={ palette === currentPalette }
+              key={ palette }
               palette={ palette }
               _setPalette={ _setPalette } />
           ))
@@ -52,4 +28,3 @@ export default Home;
 
 // TO DO:
 //  - touch event (onTouchEnd) not blurring button presses
-// Above is DUMB ------- CAN PROBS DO ALL THAT EITH CSS GRID
